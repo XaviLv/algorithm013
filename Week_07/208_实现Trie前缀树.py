240372d0b393d4f3cf6ca20df7114a0ee76fadd8
MacBook Pro <macbookpro@MacBookdeMacBook-Pro.local>
2020-09-10 08:48:21 +0800

    *: update readme

---
+++ Week_07/208_实现Trie前缀树.py
@@ -0,0 +1,42 @@
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = dict()
+        self.end_sign = '#'
+
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for c in word:
+            node = node.setdefault(c, {})
+        node[self.end_sign] = self.end_sign
+
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for c in word:
+            if c not in node:
+                return False
+            node = node[c]
+        return self.end_sign in node
+
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for c in prefix:
+            if c not in node:
+                return False
+            node = node[c]
+        return True
